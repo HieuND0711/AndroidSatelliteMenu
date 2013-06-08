147b445cf5abf5c9ce890661274590206fb61c00
Nguyen Duy Hieu <nguyenduyhieu0711@gmail.com>
2013-06-08 17:54:32 +0700

    New

---
+++ src/com/android/satellitemenu/HomeActivity.java
@@ -0,0 +1,47 @@
+package com.android.satellitemenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.satellitemenu.library.SatelliteMenu;
+import com.android.satellitemenu.library.SatelliteMenu.SateliteClickedListener;
+import com.android.satellitemenu.library.SatelliteMenuItem;
+
+public class HomeActivity extends Activity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_home);
+        
+        SatelliteMenu menu = (SatelliteMenu) findViewById(R.id.menu);
+        
+//		  Set from XML, possible to programmatically set        
+//        float distance = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 170, getResources().getDisplayMetrics());
+//        menu.setSatelliteDistance((int) distance);
+//        menu.setExpandDuration(500);
+//        menu.setCloseItemsOnClick(false);
+//        menu.setTotalSpacingDegree(60);
+        
+        List<SatelliteMenuItem> items = new ArrayList<SatelliteMenuItem>();
+        items.add(new SatelliteMenuItem(4, R.drawable.ic_1));
+        items.add(new SatelliteMenuItem(4, R.drawable.ic_3));
+        items.add(new SatelliteMenuItem(4, R.drawable.ic_4));
+        items.add(new SatelliteMenuItem(3, R.drawable.ic_5));
+        items.add(new SatelliteMenuItem(2, R.drawable.ic_6));
+        items.add(new SatelliteMenuItem(1, R.drawable.ic_2));
+//        items.add(new SatelliteMenuItem(5, R.drawable.sat_item));
+        menu.addItems(items);        
+        
+        menu.setOnItemClickedListener(new SateliteClickedListener() {
+			
+			public void eventOccured(int id) {
+				Log.i("sat", "Clicked on " + id);
+			}
+		});
+    }
+}
